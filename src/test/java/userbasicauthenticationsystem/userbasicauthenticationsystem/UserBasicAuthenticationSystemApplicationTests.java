db258ac10da1a09f0891649b6a2730015614b5d9
rnour <riham.sadek.ext@orange.com>
2019-11-08 14:11:14 +0200

    kick off

---
+++ src/test/java/userbasicauthenticationsystem/userbasicauthenticationsystem/UserBasicAuthenticationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package userbasicauthenticationsystem.userbasicauthenticationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBasicAuthenticationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
